82e2b2a5c002930f279f3fb584e7c46c222753ae
Evocode78 <Evocode78@hotmail.com>
2017-05-25 17:12:06 +1000

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions, to solve and cache a matrix
+## Function 1 - Solves a matrix, but cache's the results
+## Function 2 - Solves, but checks for a cache, and will pick up from
+## there if a cache result exists
 
-## Write a short comment describing this function
+## Function 1 - Solving a matrix and cacheing if needed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <-function(solve) s <<- solve
+  getsolve <-function() s
+  list(set=set, get=get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## Function 2, solves x, but checks to see if already a result in prev function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("calm your farm, working hard here!")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
